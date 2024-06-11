6cdd500a348cac3d3eff582a0b9f0db336ee9ec0
Bruno Felipy <brunofelipems@outlook.com>
2024-06-11 09:46:35 -0300

    feat: add services component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FAQSection } from "@/components/layout/sections/faq";
 import { FeaturesSection } from "@/components/layout/sections/features";
 import { HeroSection } from "@/components/layout/sections/hero";
 import { PricingSection } from "@/components/layout/sections/pricing";
+import { ServicesSection } from "@/components/layout/sections/services";
 import { SponsorsSection } from "@/components/layout/sections/sponsors";
 import { TestimonialSection } from "@/components/layout/sections/testimonial";
 
@@ -13,6 +14,7 @@ export default function Home() {
       <SponsorsSection />
       <BenefitsSection />
       <FeaturesSection />
+      <ServicesSection />
       <TestimonialSection />
       <PricingSection />
       <FAQSection />
