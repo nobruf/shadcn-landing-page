4b13a47aa1c627b2aca292e4538f31d8f11c558b
Bruno Felipy <brunofelipems@outlook.com>
2024-06-18 10:20:40 -0300

    feat: :fire: add footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { BenefitsSection } from "@/components/layout/sections/benefits";
 import { FAQSection } from "@/components/layout/sections/faq";
 import { FeaturesSection } from "@/components/layout/sections/features";
+import { FooterSection } from "@/components/layout/sections/footer";
 import { HeroSection } from "@/components/layout/sections/hero";
 import { PricingSection } from "@/components/layout/sections/pricing";
 import { ServicesSection } from "@/components/layout/sections/services";
@@ -18,6 +19,7 @@ export default function Home() {
       <TestimonialSection />
       <PricingSection />
       <FAQSection />
+      <FooterSection />
     </>
   );
 }
