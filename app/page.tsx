3791c66c88759a51d1e327b1eb2686d3f41f7009
Bruno Felipy <brunofelipems@outlook.com>
2024-07-26 09:17:35 -0300

    feat: add team section

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { HeroSection } from "@/components/layout/sections/hero";
 import { PricingSection } from "@/components/layout/sections/pricing";
 import { ServicesSection } from "@/components/layout/sections/services";
 import { SponsorsSection } from "@/components/layout/sections/sponsors";
+import { TeamSection } from "@/components/layout/sections/team";
 import { TestimonialSection } from "@/components/layout/sections/testimonial";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <FeaturesSection />
       <ServicesSection />
       <TestimonialSection />
+      <TeamSection />
       <PricingSection />
       <FAQSection />
       <FooterSection />
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
   },
 };
 
