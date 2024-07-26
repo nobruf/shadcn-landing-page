aaddd2968202d4354582fa49113e164f5a51e858
Bruno Felipy <brunofelipems@outlook.com>
2024-07-26 09:21:53 -0300

    fix: hero component typo

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -21,7 +21,7 @@ export const HeroSection = () => {
 
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
-              Experiece the
+              Experience the
               <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
                 Shadcn
               </span>
