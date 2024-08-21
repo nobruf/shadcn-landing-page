7cf1a424ec386feb4e481a5a9538e6ede7234117
Bruno Felipy <brunofelipems@outlook.com>
2024-08-21 14:42:23 -0300

    feat: meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,34 @@ import { SponsorsSection } from "@/components/layout/sections/sponsors";
 import { TeamSection } from "@/components/layout/sections/team";
 import { TestimonialSection } from "@/components/layout/sections/testimonial";
 
+export const metadata = {
+  title: "Shadcn - Landing template",
+  description: "Free Shadcn landing page for developers",
+  openGraph: {
+    type: "website",
+    url: "https://github.com/nobruf/shadcn-landing-page.git",
+    title: "Shadcn - Landing template",
+    description: "Free Shadcn landing page for developers",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Shadcn - Landing template",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://github.com/nobruf/shadcn-landing-page.git",
+    title: "Shadcn - Landing template",
+    description: "Free Shadcn landing page for developers",
+    images: [
+      "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <>
