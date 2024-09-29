0264142016fc6cd71a6aa22b970c500f1c8a5352
makatadi <38482112+draJiang@users.noreply.github.com>
2024-09-29 20:23:14 +0800

    Edit the metadata.

--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,27 @@ import { SponsorsSection } from "@/components/layout/sections/sponsors";
 import { TestimonialSection } from "@/components/layout/sections/testimonial";
 
 export const metadata = {
-  title: "Shadcn - Landing template",
-  description: "Free Shadcn landing page for developers",
+  title: "Grimo - ReimagineWord Processor",
+  description: "Your All-in-one Writing Interface for Better Writing",
   openGraph: {
     type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    url: "https://www.grimo.ai/",
+    title: "Grimo - ReimagineWord Processor",
+    description: "Your All-in-one Writing Interface for Better Writing",
     images: [
       {
         url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
         width: 1200,
         height: 630,
-        alt: "Shadcn - Landing template",
+        alt: "Grimo - ReimagineWord Processor",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    site: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    site: "https://www.grimo.ai/",
+    title: "Grimo - ReimagineWord Processor",
+    description: "Your All-in-one Writing Interface for Better Writing",
     images: [
       "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
     ],
